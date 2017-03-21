b6e4ed497f83e312c746ffd35b51cea395afee21
FilipHarald <filip.harald@gmail.com>
2017-03-21 12:03:08 +0100

    Changed chache sto store in JSON instead
    Why did I even use pickle in the first place!??!

--- data-collection-tools/utils/cache.py
+++ data-collection-tools/utils/cache.py
@@ -1,5 +1,5 @@
 import os
-import pickle
+import json
 
 temp_folder_path = os.path.join(os.path.dirname(__file__), os.pardir)
 
@@ -8,15 +8,15 @@ def get(key):
     file_path = _fp(key)
     print(file_path)
     if os.path.exists(file_path):
-        with open(file_path, "rb") as file:
-            return pickle.load(file)
+        with open(file_path, "r") as file:
+            return json.load(file)
     return None
 
 
 def store(key, value):
     file_path = _fp(key)
-    with open(file_path, 'wb') as file:
-        pickle.dump(str(value), file)
+    with open(file_path, 'w') as file:
+        json.dump(value, file)
     return value
 
 def cache(func, key, **kwargs):
@@ -29,4 +29,4 @@ def cache(func, key, **kwargs):
 
 
 def _fp(key):
-    return temp_folder_path + '/temp/' + key + '.pickle'
+    return temp_folder_path + '/temp/' + key + '.json'

4c058f1801e0921b820276dc724a5de93bf217c3
FilipHarald <filip.harald@gmail.com>
2017-04-04 13:34:42 +0200

    Changed data collections to gather data for other projects

--- data-collection-tools/run_all.py
+++ data-collection-tools/run_all.py
@@ -4,7 +4,6 @@ from utils.init import authenticator, config
 
 g = authenticator.get_github()
 config.init(g)
-# config.init_2()
 
 code_contributors.run(g=g, config=config)
 other_contributors.run(g=g, config=config)
